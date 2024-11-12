82f9f365f1c7ee6806ab4dd147f24cebe8fa27f8
Jesus Navarro <jesus.navarro@col.vueling.com>
2024-11-12 14:18:28 +0100

    Web-API-CountriesNow-ExamSimulation: JsonIgnore added to year to avoid innecesary field inclusion

--- CountryFinderAPI/CountryFinderAPI.Library.Contracts/DTOs/CountryDto.cs
+++ CountryFinderAPI/CountryFinderAPI.Library.Contracts/DTOs/CountryDto.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CountryFinderAPI.Library.Contracts.DTOs
@@ -9,6 +11,8 @@ namespace CountryFinderAPI.Library.Contracts.DTOs
     public class CountryDto
     {
         public string? Name { get; set; }
+        [IgnoreDataMember]
+        [JsonIgnore]
         public int Year { get; set; }
         public long Population { get; set; }
     }
